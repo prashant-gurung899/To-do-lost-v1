9dcc148f65e7fc8d4dcab86b3046e9ced69613d1
Prashant Gurung <prasantgrg777@gmail.com>
2023-03-16 21:06:22 +0545

    INITIAL COMMIT

---
+++ app.js
@@ -0,0 +1,37 @@
+  const express = require("express");
+  const bodyParser = require("body-parser");
+
+  const app = express();
+
+  app.set('view engine', 'ejs')
+
+  app.get('/',function(req,res){
+    var today = new Date();
+    var currentday = today.getDay()  ;
+    var day = "";
+
+    if(currentday === 6){
+      day = "Saturday.";
+    }
+    else if (currentday === 5) {
+      day = "Friday.";
+    } else if (currentday === 4) {
+      day = "Thursday.";
+    }
+    else if (currentday === 3) {
+      day = "Wednesday.";
+    }  else if (currentday === 2) {
+      day = "Tuesday.";
+      }
+    else if (currentday === 1) {
+      day = "Monday.";
+    }     else{
+        day = "Sunday.";
+      }
+      res.render('list',{kindOfDay: day});
+  })
+
+
+  app.listen(3000,function(){
+    console.log('Server listening at port 3000');
+  })
