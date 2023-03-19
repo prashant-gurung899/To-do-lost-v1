f34909be676faa04c80b0e832612ca74892795e2
Prashant Gurung <prasantgrg777@gmail.com>
2023-03-19 19:58:44 +0545

    add Header and footer & work list/about page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
   const app = express();
   let items =["Buy food","Cook food"];
+  let workItems = [];
 
   app.set('view engine', 'ejs')
   app.use(bodyParser.urlencoded({extended:true}))
@@ -18,15 +19,30 @@
       month: "long"
     }
       let day = today.toLocaleDateString("en-US",options);
-
-      res.render('list',{kindOfDay: day,newListItems: items});
+      res.render('list',{listTitle: day,newListItems: items});
       // res.render('list',{});
   })
+
 app.post('/',function(req,res){
    let item = req.body.newItem;
-   items.push(item);
-  res.redirect('/');
+
+   if(req.body.list === "Work"){
+     workItems.push(item);
+     res.redirect('/work');
+   }else{
+     items.push(item);
+    res.redirect('/');
+   }
+
   // console.log(item);
+})
+
+app.get('/work',function(req,res){
+  res.render("list",{listTitle:"Work List",newListItems: workItems})
+})
+
+app.get('/about',function(req,res){
+  res.render("about")
 })
 
   app.listen(3000,function(){
