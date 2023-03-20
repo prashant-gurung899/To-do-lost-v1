2724ee247f655cbc42ca66d83b6e577f0aff9313
Prashant Gurung <prasantgrg777@gmail.com>
2023-03-20 12:30:26 +0545

    Code refactor and project finish

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-  const express = require("express");
-  const bodyParser = require("body-parser");
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
-  const app = express();
-  let items =["Buy food","Cook food"];
-  let workItems = [];
+const app = express();
+const items = ["Buy food", "Cook food"];
+const workItems = [];
 
-  app.set('view engine', 'ejs')
-  app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-  app.use(express.static("public"));
+app.use(express.static("public"));
 
+app.get('/', function(req, res) {
+  const day = date.getDate();
+  res.render('list', {
+    listTitle: day,
+    newListItems: items
+  });
+  // res.render('list',{});
+})
 
-  app.get('/',function(req,res){
-    let today = new Date();
-    let options = {
-      weekday: "long",
-      day : "numeric",
-      month: "long"
-    }
-      let day = today.toLocaleDateString("en-US",options);
-      res.render('list',{listTitle: day,newListItems: items});
-      // res.render('list',{});
-  })
+app.post('/', function(req, res) {
+  const item = req.body.newItem;
 
-app.post('/',function(req,res){
-   let item = req.body.newItem;
-
-   if(req.body.list === "Work"){
-     workItems.push(item);
-     res.redirect('/work');
-   }else{
-     items.push(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
     res.redirect('/');
-   }
-
+  }
   // console.log(item);
 })
 
-app.get('/work',function(req,res){
-  res.render("list",{listTitle:"Work List",newListItems: workItems})
+app.get('/work', function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  })
 })
 
-app.get('/about',function(req,res){
+app.get('/about', function(req, res) {
   res.render("about")
 })
 
-  app.listen(3000,function(){
-    console.log('Server listening at port 3000');
-  })
+app.listen(3000, function() {
+  console.log('Server listening at port 3000');
+})
